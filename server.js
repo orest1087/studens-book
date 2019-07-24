409e822d33d6cdb5464e889a1d439a9fc0418d6f
k2 <k2@mail.local>
2019-07-24 20:11:17 +0300

    Created module connectDB  (connect to database)

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect to MongoDB (Atlas)
+connectDB();
 
 app.get("/", (req, res) => res.send(`API rinning`));
 const PORT = process.env.PORT || 3000;
