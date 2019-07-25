8ecb21eb5d242748ae6b7f80bd521c23ca93a8bf
k2 <k2@mail.local>
2019-07-25 12:01:01 +0300

    Added base routes

--- server.js
+++ server.js
@@ -9,4 +9,11 @@ connectDB();
 app.get("/", (req, res) => res.send(`API rinning`));
 const PORT = process.env.PORT || 3000;
 
+//Define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
