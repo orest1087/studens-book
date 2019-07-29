5c2e395c7dd8bb3968307381278567de96701049
k2 <k2@mail.local>
2019-07-29 16:29:56 +0300

    connect auth middleware with auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
