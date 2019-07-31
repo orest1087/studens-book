e49501ad6c3d2b71f6d158fd3eaf3dc716eaaa70
k2 <k2@mail.local>
2019-07-31 16:49:19 +0300

    Added experience api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,57 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+// Add experience
+
+router.put(
+  "/experience",
+  auth,
+  [
+    check("title", "Title is required")
+      .not()
+      .isEmpty(),
+    check("company", "Company is required")
+      .not()
+      .isEmpty(),
+    check("from", "From date is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      decription
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      decription
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
 module.exports = router;
